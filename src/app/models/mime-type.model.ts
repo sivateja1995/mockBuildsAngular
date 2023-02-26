21ff82ae75f0a4937594235738df3f8ee3eeaf2b
Dwadasi Siva Teja <siva.teja@inncrewin.com>
2023-02-26 22:21:46 +0530

    added the changes

--- src/app/models/mime-type.model.ts
+++ src/app/models/mime-type.model.ts
@@ -1,5 +1,11 @@
 export const ImageMimeTypes = {
-  title: 'Image/Video files',
+  title: 'Image files',
+  extensions: 'jpg,gif,png,jpeg,jfif,webp,bmp,tiff',
+};
+
+
+export const VideosMimeTypes = {
+  title: 'Video files',
   extensions:
-    'jpg,gif,png,jpeg,jfif,webp,bmp,tiff,3gp,mov,avi,wmv,flv,mp4,mp3,hevc,ogg,ogv,webm,mkv,asf,f4v,m2v,m4v,mjpeg,mpg,m2ts',
+    '3gp,mov,avi,wmv,flv,mp4,mp3,hevc,ogg,ogv,webm,mkv,asf,f4v,m2v,m4v,mjpeg,mpg,m2ts',
 };
