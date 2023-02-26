0a1356b2ab8bfef8aa0a688ffbf57afc54cdc3dc
Dwadasi Siva Teja <siva.teja@inncrewin.com>
2023-02-26 13:02:28 +0530

    created the basic project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadsModule } from './modules/uploads/uploads.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/upload',
+    loadChildren: () =>
+      import('./modules/uploads/uploads.module').then((m) => m.UploadsModule),
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/uploads/uploads.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageuploadComponent } from './imageupload/imageupload.component';
+
+
+
+@NgModule({
+  declarations: [
+    ImageuploadComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UploadsModule { }
